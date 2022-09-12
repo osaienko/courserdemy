10318f5dada83cfb40e6dd16cb3175108d5bc0d7
Oleh Saienko <saenko.oleg1993@gmail.com>
2022-09-12 18:26:02 +0300

    course - user relation; rails g migration AddUserToCourses user:belongs_to rake db:drop db:create db:migrate db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@admin.com', password: 'admin@admin.com', password_confirmation: 'admin@admin.com')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
