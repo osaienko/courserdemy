f1d1f6cb5bdf95adec37fa4850af1500c1b5962a
Oleh Saienko <saenko.oleg1993@gmail.com>
2022-09-13 20:09:22 +0300

    add fields to the courses model, new migration; rails g migration add_fields_to_courses short_description:text language:string level:string price:integer rake db:migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(email: 'admin@admin.com', password: 'admin@admin.com', password_confirmation: 'admin@admin.com')
+# User.create!(email: 'admin@admin.com', password: 'admin@admin.com', password_confirmation: 'admin@admin.com')
 
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
     description: Faker::TvShows::BrooklynNineNine.quote,
-    user_id: User.first.id
+    user_id: User.first.id,
+    short_description: Faker::Quote.famous_last_words,
+    language: Faker::ProgrammingLanguage.name,
+    level: 'Beginner',
+    price: Faker::Number.between(from: 1000, to: 20000)
   }])
 end
