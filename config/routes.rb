9fbca4df96da56e9e20c01ed647a13fb5924d143
Oleh Saienko <saenko.oleg1993@gmail.com>
2022-09-14 23:56:40 +0300

    edit roles

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
   get "static_pages/landing_page"
   get "privacy_policy", to: "static_pages#privacy_policy"
   get 'home/activity'
